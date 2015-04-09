d598a7bcc0b0efa0882b03612113024228d09278
SAMUAN <asalvadormu@gmail.com>
2015-04-09 13:43:59 +0200

    entrenamiento red neuronal

--- src/redneuronal/RedNeuronal.java
+++ src/redneuronal/RedNeuronal.java
@@ -6,8 +6,15 @@
 package redneuronal;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -30,7 +37,7 @@ public class RedNeuronal {
         
         File archivo=new File(archivoEntrada);
         String linea;
-        
+        LinkedList listaDatos=new LinkedList();
         try{
             BufferedReader br=new BufferedReader( new FileReader (archivo));
             while((linea=br.readLine())!=null){
@@ -40,14 +47,55 @@ public class RedNeuronal {
                 for(int i=0;i<valores.length;i++){
                     valoresD[i]=Double.parseDouble(valores[i]);
                 }
-                
-                           
+                listaDatos.add(valoresD);       
             }
         }catch(Exception e){
             e.printStackTrace();
         }
 
+        Red red=new Red();
+        red.iniciarRed(1, 8, 7, 4);
+        double[][] datosPaEntrena=new double[listaDatos.size()][12];
+        for(int j=0;j<listaDatos.size();j++){
+            datosPaEntrena[j]=(double[])listaDatos.get(j);
+        }        
+        red.setVector_datos(datosPaEntrena);
+        red.entrenamiento();
         
+        
+        double[][] sinapsisA= red.getSinapsisA();
+        double[][] sinapsisB= red.getSinapsisB();
+        
+        File archSalida=new File(archivoSalida);
+      
+        String textoArchivo="Pesos entrada capa oculta \r\n";
+        for(int i=0;i<sinapsisA.length;i++) {
+            for(int j=0;j<sinapsisA[0].length;j++){
+                textoArchivo += ""+sinapsisA[i][j]+", " ;
+            }
+            textoArchivo += "\r\n";
+        }
+        textoArchivo += "Pesos capa oculta capa salida \r\n";
+        for(int i=0;i<sinapsisB.length;i++) {
+            for(int j=0;j<sinapsisB[0].length;j++){
+                textoArchivo += ""+sinapsisB[i][j]+", " ;
+            }
+            textoArchivo += "\r\n";
+        }
+        
+        
+        //escritura en fichero nuevo
+        try {
+            PrintWriter writer = new PrintWriter(
+                    new BufferedWriter(
+                    new FileWriter(archSalida,false)));
+            writer.println(textoArchivo);
+            writer.close();  
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+            
+                
     }
     
     
